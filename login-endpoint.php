a708549e28ae60e09aa4d23477365996ca63e799
Paul Sho <Cyberkiller.CK@gmail.com>
2024-08-11 18:23:12 +0800

    CREATED THIS SHIT

---
+++ login-endpoint.php
@@ -0,0 +1,64 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+require "conn.php";
+
+class auth_endpoint
+{
+
+    private $conn;
+
+    public function __construct()
+    {
+        $this->conn = DatabaseConnection::getInstance()->getConnection();
+    }
+
+    public function login($json)
+    {
+        try {
+            $json = json_decode($json, true);
+            $sql = "SELECT id, username, role FROM `users` WHERE username = :username AND password = :password";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->bindParam(":username", $json["username"]);
+            $stmt->bindParam(":password", $json["password"]);
+            $stmt->execute();
+
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ($result > 0) {
+                echo json_encode(array("success" => $result));
+            } else {
+                echo json_encode(array("error" => "No such user!"));
+            }
+        } catch (PDOException $e) {
+
+            echo json_encode(array("error" => $e->getMessage()));
+        }
+    }
+
+}
+
+$auth_endpoint = new auth_endpoint();
+
+if ($_SERVER["REQUEST_METHOD"] == "GET" || $_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_REQUEST['operation']) && isset($_REQUEST['json'])) {
+        $operation = $_REQUEST['operation'];
+        $json = $_REQUEST['json'];
+
+        switch ($operation) {
+            case 'login':
+                echo $auth_endpoint->login($json);
+                break;
+
+            default:
+                echo json_encode(["error" => "Invalid operation"]);
+                break;
+        }
+    } else {
+        echo json_encode(["error" => "Missing parameters"]);
+    }
+} else {
+    echo json_encode(["error" => "Invalid request method"]);
+}
+?>
---
+++ store_operations.php
@@ -0,0 +1,66 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+require "conn.php";
+
+
+class store_operations
+{
+    private $conn;
+
+    public function __construct()
+    {
+        $this->conn = DatabaseConnection::getInstance()->getConnection();
+    }
+
+
+    public function getItem($json)
+    {
+        $json = json_decode($json, true);
+        try {
+            $sql = "SELECT * FROM `products` WHERE barcode = :barcode";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->bindParam(":barcode", $json["barcode"]);
+            $stmt->execute();
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($result) {
+                echo json_encode($result);
+            } else {
+                echo json_encode(array("error" => "No such item"));
+            }
+        } catch (PDOException $e) {
+            echo json_encode(array("error" => $e->getMessage()));
+        }
+    }
+
+    
+}
+
+
+
+$store_operations = new store_operations();
+
+if ($_SERVER["REQUEST_METHOD"] == "GET" || $_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_REQUEST['operation']) && isset($_REQUEST['json'])) {
+        $operation = $_REQUEST['operation'];
+        $json = $_REQUEST['json'];
+
+        switch ($operation) {
+            case 'getItem':
+                echo $store_operations->getItem($json);
+                break;
+
+            default:
+                echo json_encode(["error" => "Invalid operation"]);
+                break;
+        }
+    } else {
+        echo json_encode(["error" => "Missing parameters"]);
+    }
+} else {
+    echo json_encode(["error" => "Invalid request method"]);
+}
+?>
